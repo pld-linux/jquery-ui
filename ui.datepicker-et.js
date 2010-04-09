7e3a6dc1b0be164f8329f66cd3507ef78bb83bff
Elan Ruusamäe <glen@pld-linux.org>
2010-04-09 09:20:54 +0000

    - save from http://g.delfi.ua/cl/js/jQuery/ui/ui.datepicker-ee.js
    Changed files:
        ui.datepicker-et.js -> 1.1

---
+++ ui.datepicker-et.js
@@ -0,0 +1,18 @@
+/* Estonian initialisation for the jQuery UI date picker plugin. */
+/* Written by Agne Lund (gness@gness.net). */
+jQuery(function($){
+    $.datepicker.regional['ee'] = {
+		closeText: 'Sulge',
+		prevText: '&laquo;Eelmine',
+		nextText: 'Järgmine&raquo;',
+		currentText: 'Täna',
+        monthNames: ['Jaanuar','Veebruar','Märts','Aprill','Mai','Juuni','Juuli','August','September','Oktoober','November','Detsember'],
+        monthNamesShort: ['Jaan','Veebr','Mär','Apr','Mai','Jun',
+        'Jul','Aug','Sept','Okt','Nov','Dets'],
+		dayNamesShort: ['P','E','T','K','N','R','L'],
+		dayNames: ['Pühapäev','Esmaspäev','Teisipäev','Kolmapäev','Neljapäev','Reede','Laupäev'],
+		dayNamesMin: ['P','E','T','K','N','R','L'],
+        dateFormat: 'dd.mm.yy', firstDay: 1,
+		isRTL: false};
+    $.datepicker.setDefaults($.datepicker.regional['ee']);
+});
